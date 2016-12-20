b323b2e9e3b3726587ccccddc6f14494e835be86
Matthieu <m2600@orange.fr>
2016-12-20 18:07:22 +0100

    modification du titre du fichier header.php

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
    <!-- <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"> -->
    <title>Bibliothèque musicale</title>
    <div class="container-fluid">
-   <h1>Bibliothèque musicale HFamily</h1>
+   <h1>Bibliothèque musicale</h1>
 	<div class="btn-group">
 	<button class="btn btn-default btn-lg dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Menu
 	<span class="caret"></span>
