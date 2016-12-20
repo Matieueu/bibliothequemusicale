e022f3d422939a004436d4ff2b79000e2cf263d0
Matthieu <m2600@oranfe.fr>
2016-12-20 16:50:10 +0100

    Premier commit bibliotheque musicale

---
+++ header.php
@@ -0,0 +1,26 @@
+<html lang="fr">
+<head>
+   <link href="../bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
+   <link href="../bootstrap-3.3.7-dist/css/bootstrap-theme.min.css" rel="stylesheet">
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+   <script src="../bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <!-- <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"> -->
+   <title>Bibliothèque musicale</title>
+   <div class="container-fluid">
+   <h1>Bibliothèque musicale HFamily</h1>
+	<div class="btn-group">
+	<button class="btn btn-default btn-lg dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Menu
+	<span class="caret"></span>
+	</button>
+	<ul class="dropdown-menu">
+		<li><a href="recherche.php">Recherche générale</a></li>
+		<li role="separator" class="divider"></li>
+		<li><a href="albums.php">Albums</a></li>
+		<li><a href="artistes.php">Artistes</a></li>
+		<li><a href="locations.php">Locations</a></li>
+	</ul>
+	</div>
+   </div>
+</head>
+</html>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include("header.php"); ?>
+<body>
+<div class="container-fluid">
+<p><br /><br /><h2>Bienvenue !</h2></p>
+</div>
+</body
+</html>
